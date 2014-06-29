f09872ca9143d58c54b99e87685827f8fd539524
Adam <adam.cragg@gmail.com>
2014-06-29 02:24:14 -0400

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,13 @@ Braggcragg::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( *.js *.css )
+   # Include all JS files, also those in subdolfer or javascripts assets folder
+ # includes for exmaple applicant.js. JS isn't the problem so the catch all works.
+ config.assets.precompile += %w(*.js)
+ # Replace %w( *.css *.js *.css.scss) with complex regexp avoiding SCSS partials compilation
+ config.assets.precompile += [/^[^_]\w+\.(css|css.scss)$/]
+ #Adding active_admin JS and CSS to the precompilation list
+ config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
