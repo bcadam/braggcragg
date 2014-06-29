b4ad32a5110c6d1d03394d6692e29473306498ad
Adam <adam.cragg@gmail.com>
2014-06-28 20:25:24 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Braggcragg::Application.config.secret_key_base = '0bed63bad5ab1d212f0cc02497c831b16d98dd583f26128403b538ed7c6bcb833de81bc6764cc1481e58ecd936f84f9b0b22c371e63048998f2fbd1935b0bbba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Braggcragg::Application.config.session_store :cookie_store, key: '_braggcragg_session'
